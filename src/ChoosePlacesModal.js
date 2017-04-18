74dc88317d86cdcf24bf34154f56adff8d5f1888
Francesco Cioria <firefelix@gmail.com>
2017-04-18 20:22:05 +0200

    move ChoosePlacesModal in its own component

---
+++ src/ChoosePlacesModal.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { t, propTypes } from 'tcomb-react';
+import debounce from 'lodash/debounce';
+import { get } from './request';
+import Dropdown from 'react-select';
+import Modal from './Modal';
+
+import 'react-select/dist/react-select.css';
+import './choosePlacesModal.css';
+
+export default class ChoosePlacesModal extends React.Component {
+
+  static propTypes = propTypes({
+    onDismiss: t.Function,
+    onSave: t.Function
+  })
+
+  state = {}
+
+  onAddPlace = (places) => this.setState({ places })
+
+  searchPlaces(input) {
+    return get('https://graph.facebook.com/search', { q: input, type: 'place' }, false);
+  }
+
+  onSave = () => this.props.onSave(this.state.places);
+
+  render() {
+    const {
+      state: { places },
+      props: { onDismiss },
+      onAddPlace,
+      onSave
+    } = this;
+
+    const loadOptions = debounce((input, cb) => {
+      if (input.length > 0) {
+        this.searchPlaces(input).then(({ data: places }) => {
+          cb(null, { options: places.map(p => ({ value: p.id, label: p.name })) });
+        })
+      } else {
+        cb(null, { options: [] });
+      }
+    }, 200);
+
+    return (
+      <Modal className='choose-places-modal' onDismiss={onDismiss} title='Choose your places'>
+        <Dropdown.Async multi value={places} loadOptions={loadOptions} onChange={onAddPlace} />
+        <button onClick={onSave} disabled={!places || places.length === 0}>
+          Start using 2NITE
+        </button>
+      </Modal>
+    )
+  }
+}
--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { t, propTypes } from 'tcomb-react';
-import debounce from 'lodash/debounce';
 import View from 'react-flexview';
-import Dropdown from 'react-select';
-import { get } from './request';
-import Modal from './Modal';
+import ChoosePlacesModal from './ChoosePlacesModal';
 
-import 'react-select/dist/react-select.css';
 import './welcomePage.css';
 
 export default class WelcomePage extends React.Component {
@@ -16,11 +12,7 @@ export default class WelcomePage extends React.Component {
   })
 
   state = {
-    showModal: false,
-    modal: {
-      options: [],
-      isLoadingOptions: false
-    }
+    showModal: false
   }
 
   openModal = () => {
@@ -31,56 +23,23 @@ export default class WelcomePage extends React.Component {
     this.setState({ showModal: false });
   }
 
-  searchPlaces(input) {
-    return get('https://graph.facebook.com/search', { q: input, type: 'place' }, false);
-  }
-
-  onAddPlace = (places) => this.setState({ places })
-
-  onSave = () => {
-    this.props.onAddPlaces(this.state.places);
-  }
-
-  templateModal({ onDismiss, searchPlaces, onAddPlace, onSave, places }) {
-    const loadOptions = debounce((input, cb) => {
-      if (input.length > 0) {
-        searchPlaces(input).then(({ data: places }) => {
-          cb(null, { options: places.map(p => ({ value: p.id, label: p.name })) });
-        })
-      } else {
-        cb(null, { options: [] });
-      }
-    }, 200);
-
-    return (
-      <Modal className='choose-places-modal' onDismiss={onDismiss} title='Choose your places'>
-        <Dropdown.Async multi value={places} loadOptions={loadOptions} onChange={onAddPlace} />
-        <button onClick={onSave} disabled={!places || places.length === 0}>
-          Start using 2NITE
-        </button>
-      </Modal>
-    )
+  onSave = (places) => {
+    this.props.onAddPlaces(places);
   }
 
   render() {
-    const { showModal, places } = this.state;
+    const { showModal } = this.state;
 
     return (
       <View className='welcome-page' hAlignContent='center'>
-        {showModal && this.templateModal({
-          places,
-          onDismiss: this.closeModal,
-          searchPlaces: this.searchPlaces,
-          onAddPlace: this.onAddPlace,
-          onSave: this.onSave
-        })}
+        {showModal && <ChoosePlacesModal onDismiss={this.closeModal} onSave={this.onSave} />}
         <View className='body' grow column>
           <h1>2NITE</h1>
           <p>Choose your places to get started.</p>
           <button onClick={this.openModal}>
             Choose Places
           </button>
-          <img className='frame' src={process.env.NODE_ENV === 'development' ? '/welcome.jpg' : '/2NITE/welcome.jpg'} />
+          <img className='frame' alt='' src={process.env.NODE_ENV === 'development' ? '/welcome.jpg' : '/2NITE/welcome.jpg'} />
         </View>
       </View>
     );
