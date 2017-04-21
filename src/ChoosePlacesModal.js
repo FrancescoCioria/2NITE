1b2c1b9d71d33def17a94f4d7ebfc5986eed9b40
Francesco Cioria <firefelix@gmail.com>
2017-04-22 01:20:09 +0200

    improve ChoosePlacesModal

--- src/ChoosePlacesModal.js
+++ src/ChoosePlacesModal.js
@@ -3,6 +3,7 @@ import { t, propTypes } from 'tcomb-react';
 import debounce from 'lodash/debounce';
 import { get } from './request';
 import Dropdown from 'react-select';
+import View from 'react-flexview';
 import Modal from './Modal';
 
 import 'react-select/dist/react-select.css';
@@ -23,10 +24,12 @@ export default class ChoosePlacesModal extends React.Component {
     places: this.props.defaultPlaces ? this.props.defaultPlaces.map(p => ({
       value: p.id,
       label: p.name
-    })) : undefined
+    })) : []
   }
 
-  onAddPlace = (places) => this.setState({ places })
+  onAddPlace = (place) => this.setState({ places: [place].concat(this.state.places) })
+
+  onRemovePlace = (p) => this.setState({ places: this.state.places.filter(place => place.value !== p.value) })
 
   searchPlaces(input) {
     return get('https://graph.facebook.com/search', { q: input, type: 'place' }, false);
@@ -34,11 +37,27 @@ export default class ChoosePlacesModal extends React.Component {
 
   onSave = () => this.props.onSave(this.state.places);
 
+  templateAddedPlaces({ places, onRemovePlace }) {
+    return (
+      <View className='added-places' column grow>
+        {places.map(p => (
+          <View className='added-place' shrink={false}>
+            {p.label}
+            <View className='remove' marginLeft='auto' onClick={() => onRemovePlace(p)}>
+              Remove
+            </View>
+          </View>
+        ))}
+      </View>
+    )
+  }
+
   render() {
     const {
       state: { places },
       props: { onDismiss },
       onAddPlace,
+      onRemovePlace,
       onSave
     } = this;
 
@@ -54,7 +73,8 @@ export default class ChoosePlacesModal extends React.Component {
 
     return (
       <Modal className='choose-places-modal' onDismiss={onDismiss} title='Choose your places'>
-        <Dropdown.Async multi value={places} loadOptions={loadOptions} onChange={onAddPlace} />
+        <Dropdown.Async loadOptions={loadOptions} onChange={onAddPlace} />
+        {this.templateAddedPlaces({ places, onRemovePlace })}
         <button onClick={onSave} disabled={!places || places.length === 0}>
           Start using 2NITE
         </button>
